f38113de5a199909b4c1df4ea6d7abc60b8cc9f6
Winslow Dalpe <rwdalpe@gmail.com>
2015-02-16 11:22:59 -0800

    better menu styling and graceful fallback for nojs

---
+++ src/common/js/menu.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	$("article.book > div.toc").slicknav({
+		label : "",
+		allowParentLinks : true,
+		init : function() {
+			$("div.toc").addClass("js-on");
+			$("article.book").addClass("js-on");
+		},
+		open : function(trigger) {
+			if (trigger[0].className.indexOf("slicknav_btn") > -1) {
+				$(".slicknav_menu").css("height", "60%");
+			}
+		},
+		close : function(trigger) {
+			if (trigger[0].className.indexOf("slicknav_btn") > -1) {
+				$(".slicknav_menu").css("height", "");
+			}
+		}
+	});
+});
