6ee452e371cf4ce2c5c779ca666b53e258bdb6c7
Winslow Dalpe <rwdalpe@gmail.com>
2015-07-15 01:08:51 -0700

    Trying out automated deployment with Travis!

---
+++ lib/deploy-pages.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+cd ../output/html/single-file/
+git init
+
+git config user.name "Travis CI"
+git config user.email "rwdalpe@gmail.com"
+
+git add .
+git commit -m "Deploy to GitHub Pages"
+
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
